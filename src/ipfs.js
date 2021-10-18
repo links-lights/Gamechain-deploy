edfc33f12c8e6def8e44d87de6c47bb9d27fabad
emil <poolsar.42@gmail.com>
2021-10-19 00:05:56 +0300

    secret and id

--- src/ipfs.js
+++ src/ipfs.js
@@ -3,11 +3,11 @@ import * as IPFS from "ipfs";
 console.log(process.env);
 console.log(window);
 
+const PROJECT_ID = "1zh41fxyr27yuVrJ8wQirctUJGx";
+const PROJECT_SECRET = "917a314e9e48fed847f60e0096f84d0a";
+
 const auth =
-  "Basic " +
-  Buffer.from(
-    process.env.PROJECT_ID + ":" + process.env.PROJECT_SECRET
-  ).toString("base64");
+  "Basic " + Buffer.from(PROJECT_ID + ":" + PROJECT_SECRET).toString("base64");
 
 const ipfsOptions = {
   EXPERIMENTAL: {
