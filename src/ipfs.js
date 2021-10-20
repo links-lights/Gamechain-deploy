c801b74f26a870ba54c7e55b53f7584a477009b3
emil <poolsar.42@gmail.com>
2021-10-20 21:03:25 +0300

    orbit. one more chance

--- src/ipfs.js
+++ src/ipfs.js
@@ -8,6 +8,15 @@ const ipfsOptions = {
   host: "ipfs.infura.io",
   port: 5001,
   protocol: "https",
+  config: {
+    Addresses: {
+      Swarm: [
+        "/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/",
+        "/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/",
+        "/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/",
+      ],
+    },
+  },
 };
 
 const ipfs = IPFS.create(ipfsOptions);
