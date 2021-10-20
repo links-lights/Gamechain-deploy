dcffe3c88cbad0f5eaf256a44b8097da7fd32755
emil <poolsar.42@gmail.com>
2021-10-20 20:46:43 +0300

    fix: minor change

--- src/ipfs.js
+++ src/ipfs.js
@@ -5,15 +5,9 @@ const ipfsOptions = {
   EXPERIMENTAL: {
     pubsub: true,
   },
-  config: {
-    Addresses: {
-      Swarm: [
-        "/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/",
-        "/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/",
-        "/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/",
-      ],
-    },
-  },
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https",
 };
 
 const ipfs = IPFS.create(ipfsOptions);
